e95361ba94d6c850de8e18f272cee08ccdf3acfb
Miha Meden <meden.miha@gmail.com>
2021-01-07 18:22:10 +0100

    before

--- logic.js
+++ logic.js
@@ -86,7 +86,7 @@ function main() {
 
   {
     const gltfLoader = new GLTFLoader();
-    gltfLoader.load('datoteke/panthera-full.glb', (gltf) => {
+    gltfLoader.load('datoteke/test-omarice2.gltf', (gltf) => {
       const root = gltf.scene;
       scene.add(root);
 
